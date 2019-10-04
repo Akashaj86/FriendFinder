6356a6b32a7ffa83c9b75c0b7186a399b243d488
Ashlie Johnson <ashlie86.2003@yahoo.com>
2019-10-04 18:55:12 -0400

    setup of routes n pages

---
+++ app/data/friends.js
@@ -0,0 +1,61 @@
+var friends = [
+  {
+    name: "Brady Douthit",
+    photo: "https://avatars3.githubusercontent.com/u/47122328?s=460&v=4",
+    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
+  },
+
+  {
+    name: "Charles Mercer",
+    photo: "https://avatars3.githubusercontent.com/u/48850844?s=460&v=4",
+    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
+  },
+
+  {
+    name: "Jason Novak ",
+    photo: "img.png",
+    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
+  },
+
+  {
+    name: "Anthony Anderson",
+    photo: "img.png",
+    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
+  },
+
+  {
+    name: "Will Small",
+    photo: "img.png",
+    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
+  },
+
+  {
+    name: "Clara Garcia",
+    photo: "img.png",
+    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
+  },
+
+  {
+    name: "Amber Charnley",
+    photo: "img.png",
+    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
+  },
+
+  {
+    name: "Victoria Sharpe",
+    photo: "img.png",
+    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
+  },
+
+  {
+    name: "Ryan Lester",
+    photo: "img.png",
+    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
+  },
+
+  {
+    name: "John Faulk",
+    photo: "img.png",
+    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
+  }
+];
