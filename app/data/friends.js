69a8096ec88bf9537f87872612cd2affd95777ea
Ashlie Johnson <ashlie86.2003@yahoo.com>
2019-10-06 03:40:28 -0400

    added tracked down pics

--- app/data/friends.js
+++ app/data/friends.js
@@ -13,49 +13,49 @@ var friends = [
 
   {
     name: "Jason Novak ",
-    photo: "img.png",
+    photo: "https://avatars0.githubusercontent.com/u/29254447?s=460&v=4",
     scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
   },
 
   {
     name: "Anthony Anderson",
-    photo: "img.png",
+    photo: "https://avatars3.githubusercontent.com/u/49754765?s=460&v=4",
     scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
   },
 
   {
-    name: "Will Small",
-    photo: "img.png",
+    name: "Curtis Jochen",
+    photo: "imhttps://avatars2.githubusercontent.com/u/47367085?s=460&v=4g.png",
     scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
   },
 
   {
-    name: "Clara Garcia",
-    photo: "img.png",
+    name: "Greg Smith",
+    photo: "https://avatars1.githubusercontent.com/u/24413964?s=460&v=4",
     scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
   },
 
   {
     name: "Amber Charnley",
-    photo: "img.png",
+    photo: "https://media.licdn.com/dms/image/C4D03AQFtSWEERghaLQ/profile-displayphoto-shrink_200_200/0?e=1576108800&v=beta&t=0rAlJ0TxcZhidkgOQnec8yrOt4A94pLYZIksQ_GETUI",
     scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
   },
 
   {
     name: "Victoria Sharpe",
-    photo: "img.png",
+    photo: "https://media.licdn.com/dms/image/C4E03AQGMY4EfJZ6OlA/profile-displayphoto-shrink_200_200/0?e=1576108800&v=beta&t=3wxF8PGSp5g_XEd8sJF7-QwGwljbeh-6TpYeD5Qpedw",
     scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
   },
 
   {
-    name: "Ryan Lester",
-    photo: "img.png",
+    name: "Cory",
+    photo: "https://avatars0.githubusercontent.com/u/22305668?s=460&v=4",
     scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
   },
 
   {
     name: "John Faulk",
-    photo: "img.png",
+    photo: "https://files.slack.com/files-pri/TJ6EYEPQ8-FP2QW97N0/screenshot_20191006-033716.jpg",
     scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
   }
 ];
